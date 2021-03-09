1bb1826895c7f80402c190c13bbadebe1ec9f109
Devy Leskens <devy.leskens@student.ucll.be>
2021-03-09 17:17:10 +0100

    03-regex [1-20]

--- 02-advanced-python/02-list-comprehensions/03-slug/student.py
+++ 02-advanced-python/02-list-comprehensions/03-slug/student.py
@@ -1,1 +1,7 @@
 # Write your code here
+def slug(name):
+    splitname = name.split(' ')
+    firstname = splitname[0]
+    lastname = splitname[1:]
+
+    return '-'.join(s.lower() for s in lastname + [firstname])
